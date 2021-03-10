abd6eaca8d845deb0f13f269af29a1ca0c3f726d
sahracosta <sahracosta@gmail.com>
2021-03-10 16:40:36 +0100

    j

--- Assets/Scripts/gemeinsamenFile.cs
+++ Assets/Scripts/gemeinsamenFile.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class gemeinsamenFile : MonoBehaviour
 {
-    int test;
+    int i = 0;
     int nochEinTest;
     // Start is called before the first frame update
     void Start()
     {
-        int i = 0;
+        
     }
 
     // Update is called once per frame

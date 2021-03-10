7a3bfb848c728057d0f34dcba0be8b5e00ed8e87
sahracosta <sahracosta@gmail.com>
2021-03-10 17:06:19 +0100

    test

--- Assets/Scripts/gemeinsamenFile.cs
+++ Assets/Scripts/gemeinsamenFile.cs
@@ -42,9 +42,10 @@ public class gemeinsamenFile : MonoBehaviour
         print("Halloooo");
     }
 
-    private void DoSomething()
+    private void JustDidSomething()
     {
         int habKeinPlanWasIchHierTue;
+        
 
     }
 

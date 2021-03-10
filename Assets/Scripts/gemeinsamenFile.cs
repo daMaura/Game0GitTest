08fc7f82c3135e0fb150209f32df73bad0b86868
BlackMadRabbit <millerfreddy@googlemail.com>
2021-03-10 16:31:55 +0100

    letsGo

--- Assets/Scripts/gemeinsamenFile.cs
+++ Assets/Scripts/gemeinsamenFile.cs
@@ -7,7 +7,7 @@ public class gemeinsamenFile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Freddy war hier!!!!")
     }
 
     // Update is called once per frame
